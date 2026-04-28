b6425314c12c03d86a20ad574802cbb5329bbeb1
Racheal I. Ogunmodede <razylove02@gmail.com>
2026-04-28 12:13:45 +0100

    fix: update import path for useRouter from next/navigation

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { login } from "@/lib/auth";
 import Link from "next/link";
 import { ROUTES } from "@/lib/constants";
